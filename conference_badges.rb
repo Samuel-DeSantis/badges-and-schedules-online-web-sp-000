043b2bf0408738c531ef0c500bb352c7799b5a32
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 17:54:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.each { |name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+  names.collect { |name| "Hello, #{name}! You'll be assigned to room #{names.index(name)}!"}
+end
+
+def printer(names)
+  puts batch_badge_creator(names)
+  puts assign_rooms(names)
+end
